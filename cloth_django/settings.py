1446d9b6f3d0b020d060ffde08b4ef0dfbaab87a
Mark Holmes <holmesmarkr@johndeere.com>
2020-02-12 14:19:22 -0600

    turning debugging off

--- cloth_django/settings.py
+++ cloth_django/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6-i28-m)=i5+_n%y(d$bx$r870slrx*v6cx9^c=)sg2wc0e=bo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 #ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1',            
